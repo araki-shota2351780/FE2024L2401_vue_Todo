777ef186fcce39726659ca91af7bd7c77e922987
araki-shota2351780 <araki.shota0227@gmail.com>
2024-12-10 01:54:51 +0000

    css

--- Todo/script/script.js
+++ Todo/script/script.js
@@ -2,16 +2,25 @@ new Vue({
   el: '#app',
   data() {
     return {
-      //データの準備
-
+      newTodo: '', // 入力中のタスク
+      todos: [] // タスクのリスト
     };
   },
   methods: {
-    //inputTextメソッドを追加
-
-    //addTodoメソッドを追加
-
-    //resetTextメソッドを追加
-    
+    // inputTextメソッドを追加
+    inputText(event) {
+      this.newTodo = event.target.value; // 入力イベントの値をnewTodoに設定
+    },
+    // addTodoメソッドを追加
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push(this.newTodo.trim());
+        this.newTodo = ''; // 入力欄をクリア
+      }
+    },
+    // resetTextメソッドを追加
+    resetText() {
+      this.newTodo = ''; // 入力欄をクリア
+    }
   }
 });

1c587cbb03a9f6aaf688f13e3dadce22da0ca5ff
araki-shota2351780 <araki.shota0227@gmail.com>
2024-12-16 01:16:23 +0000

    css

--- Todo/script/script.js
+++ Todo/script/script.js
@@ -3,24 +3,46 @@ new Vue({
   data() {
     return {
       newTodo: '', // 入力中のタスク
-      todos: [] // タスクのリスト
+      todos: [], // タスクのリスト
     };
   },
-  methods: {
-    // inputTextメソッドを追加
-    inputText(event) {
-      this.newTodo = event.target.value; // 入力イベントの値をnewTodoに設定
+  computed: {
+    // 完了タスク
+    doneTodo() {
+      return this.todos.filter((todo) => todo.isDone === true);
     },
-    // addTodoメソッドを追加
+    // 未完了タスク
+    incompleteTodo() {
+      return this.todos.filter((todo) => todo.isDone === false);
+    },
+  },
+  methods: {
+    // タスク追加
     addTodo() {
       if (this.newTodo.trim() !== '') {
-        this.todos.push(this.newTodo.trim());
+        this.todos.push({
+          id: Date.now(),
+          text: this.newTodo.trim(),
+          isDone: false,
+        });
         this.newTodo = ''; // 入力欄をクリア
       }
     },
-    // resetTextメソッドを追加
-    resetText() {
-      this.newTodo = ''; // 入力欄をクリア
-    }
-  }
+    // タスク削除
+    deleteTodo(id) {
+      const index = this.getIndexBy(id);
+      this.todos.splice(index, 1);
+    },
+    // 完了・未完了の切り替え
+    toggleIsDone(id) {
+      const index = this.getIndexBy(id);
+      this.todos[index].isDone = !this.todos[index].isDone;
+    },
+    // IDからインデックス取得
+    getIndexBy(id) {
+      const filteredTodo = this.todos.filter((todo) => todo.id === id)[0];
+      const index = this.todos.indexOf(filteredTodo);
+      return index;
+    },
+  },
 });
